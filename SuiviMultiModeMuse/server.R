1a7dcafec50d4308e1fca628a99597c80f332e71
Cédric Tassart <cedric.tassart@insee.fr>
2018-01-04 09:14:41 +0100

    Commit 3

--- SuiviMultiModeMuse/server.R
+++ SuiviMultiModeMuse/server.R
@@ -10,63 +10,63 @@ server <- function(input, output) {
   
   Donnees <- reactive({
     # ----------------------------------------------------------------------------------------
-    # config = httr::config(ssl_verifypeer = 0L)
-    # set_config(config)
-    # Donnees <- httr::GET( "https://qfloccapi3lht01.ad.insee.intra/loccapi3g/rest/multimode/suivi", use_proxy(url = ""), verbose() )
-    # Donnees <- fromJSON(content(Donnees, "text"))
-    # Donnees$NumSemaine <- paste0("N°",format(as.Date(Donnees$semainereference), "%U"))
-    # Donnees$IdentifEnqueteur <- paste0(Donnees$enqueteurnom," ",Donnees$enqueteurprenom," ",Donnees$enqueteuridep)
-    # 
-    # # EN ATTENDANT D'AVOIR LES VERITABLES VARIABLES RELATIVES AU POLE EEC
-    # Donnees$EEC_refus <- Donnees$refus
-    # Donnees$EEC_horschamp <- Donnees$horschamp
+    config = httr::config(ssl_verifypeer = 0L)
+    set_config(config)
+    Donnees <- httr::GET( "https://qfloccapi3lht01.ad.insee.intra/loccapi3g/rest/multimode/suivi", use_proxy(url = ""), verbose() )
+    Donnees <- fromJSON(content(Donnees, "text"))
+    Donnees$NumSemaine <- paste0("N°",format(as.Date(Donnees$semainereference), "%U"))
+    Donnees$IdentifEnqueteur <- paste0(Donnees$enqueteurnom," ",Donnees$enqueteurprenom," ",Donnees$enqueteuridep)
+
+    # EN ATTENDANT D'AVOIR LES VERITABLES VARIABLES RELATIVES AU POLE EEC
+    Donnees$EEC_refus <- Donnees$refus
+    Donnees$EEC_horschamp <- Donnees$horschamp
     # ----------------------------------------------------------------------------------------
     
-    # LORSQUE PBM DE CONNEXION AU WEBSERVICE
-    Donnees <- readRDS("Temp/Donnees.rds")
+    # !!! LORSQUE PBM DE CONNEXION AU WEBSERVICE
+    # Donnees <- readRDS("Temp/Donnees.rds")
+    # ----------------------------------------------------------------------------------------
     
     return(Donnees)
   })
   
   output$Donnees <- renderDataTable({
     Export_Donnees <- Donnees()
-    # A compléter. Attention : utilisation des noms dans Stats1_DEM ... A adpater !!!
     names(Export_Donnees) <- c("Numéro Grappe","nolog","nole","semaine de référence","Début collecte","Fin collecte","Nom enquêteur"
                                ,"Prénom","IDEP","Pôle Gestion","Au moins 1 contact (enq)","Questionnaire démarré (enq)","Finalisé (enq)","Refus (enq)"
                                ,"Hors champ (enq)","Validé (enq)","Encours (Web)","Validé (Web)","Numéro de semaine","Identif enquêteur","VarTemp : Refus (Pôle EEC)","VarTemp : Hors champ (Pôle EEC)")
     Export_Donnees <- Export_Donnees[,c(10,20,7:9,1,19,4:6,2,3,11:18,21,22)]
-     datatable(
+     
+    # Création de fonctions spécifiques au tableau : boutons affichage et 'désaffichage' de colonnes + Réorganisation des colonnes + Sélection de cellules
+    datatable(
       Export_Donnees, rownames = T,
       extensions = c('Buttons','ColReorder','KeyTable'),
       options = list(dom = 'Bfrtip',
                      colReorder = TRUE,
                      keys = TRUE,
-                     buttons = list(list(extend = 'colvis', columns = c(3:5,8:22))))
-    )
-       
-    # LORSQUE PBM DE CONNEXION AU WEBSERVICE
-    # Donnees()
+                     buttons = list(list(extend = 'colvis', columns = c(3:5,8:22))))) %>% 
+      formatStyle(c(1:2,6:7), backgroundColor = 'LightBlue') 
   })
   
   
   output$Afficher_ChxReg <- renderUI({
     # ----------------------------------------------------------------------------------------
-    # config = httr::config(ssl_verifypeer = 0L)
-    # set_config(config)
-    # Reg <- httr::GET( "https://qfloccapi3lht01.ad.insee.intra/loccapi3g/rest/multimode/etab", use_proxy(url = ""), verbose() )
-    # Reg <- fromJSON(content(Reg, "text"))
-    # 
-    # # POUR LES TESTS, EN ATTENDANT D'AVOIR PLUS DE DONNEES
-    # ListReg <- as.data.frame(c("Ensemble des régions","ETB de LILLE","ETB de TOULOUSE","ETB de TOULOUSE",Reg))
-    # # ListReg <- as.data.frame(c("Ensemble des régions",Reg))
-    # 
-    # names(ListReg) <- "NomsReg"
-    # ListReg <- as.character(unique(ListReg$NomsReg))
+    config = httr::config(ssl_verifypeer = 0L)
+    set_config(config)
+    Reg <- httr::GET( "https://qfloccapi3lht01.ad.insee.intra/loccapi3g/rest/multimode/etab", use_proxy(url = ""), verbose() )
+    Reg <- fromJSON(content(Reg, "text"))
+
+    # POUR LES TESTS, EN ATTENDANT D'AVOIR PLUS DE DONNEES
+    ListReg <- as.data.frame(c("Ensemble des régions","ETB de LILLE","ETB de TOULOUSE","ETB de TOULOUSE",Reg))
+    # ListReg <- as.data.frame(c("Ensemble des régions",Reg))
+
+    names(ListReg) <- "NomsReg"
+    ListReg <- as.character(unique(ListReg$NomsReg))
     # ----------------------------------------------------------------------------------------
     
-    # LORSQUE PBM DE CONNEXION AU WEBSERVICE
-    ListReg <- readRDS("Temp/ListReg.rds")
-    # ListReg
+    # !!! LORSQUE PBM DE CONNEXION AU WEBSERVICE
+    # ListReg <- readRDS("Temp/ListReg.rds")
+
+    # ----------------------------------------------------------------------------------------
     selectInput("ChxReg",
                 "Sélectionnez une ou plusieurs régions",
                 choices = ListReg,
@@ -91,9 +91,11 @@ server <- function(input, output) {
                 Total_Refus_HC=Enq_Refus_HC+EEC_Refus_HC,
                 Total_FA=Enq_AuMoinsUn+Enq_Demarre+Enq_Finalise+Enq_Refus_HC+Enq_Valide+Web_EnCours+Web_Valide+EEC_Refus_HC,
                 Reste=Total_FA-Total_Valide-Total_Refus_HC)
+    
     names(Stats1_DEM) <- c("Enquêteur","Grappe","Semaine de référence","Au moins un essai de contact","Questionnaire démarré","Finalisation","Refus / Hors champ","Validé","Web : en cours","web : validé","Pôle : Refus/Hors champ","Total validé","Total Refus/Hors champ","Total des FA", "Reste à traiter")
     Stats1_DEM <- Stats1_DEM[,c(1:3,14,4:13,15)]
     
+    # Création de fonctions spécifiques au tableau : boutons Copier, Imprimer et télécharger + Réorganisation des colonnes + Sélection de cellules
     datatable(Stats1_DEM,
               extensions = c('Buttons','ColReorder','KeyTable'),
               options =list(dom = 'Bfrtip',
@@ -101,8 +103,7 @@ server <- function(input, output) {
                             colReorder = TRUE,
                             buttons = list('copy', 'print',list(extend = 'collection',
                                                                 buttons = c('csv', 'excel', 'pdf'),
-                                                                text = 'Download')))
-    ) %>% 
+                                                                text = 'Download')))) %>% 
       formatStyle(c(1:3,10,15), backgroundColor = 'LightBlue') 
   })
 }
